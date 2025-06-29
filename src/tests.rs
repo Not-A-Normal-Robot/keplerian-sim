4afa2e497a74a03d32d2977000ca2a0c99c8796d
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2025-06-30 00:29:10 +0700

    Fix incorrect units used in unit tests

--- src/tests.rs
+++ src/tests.rs
@@ -68,7 +68,11 @@ fn poll_orbit(orbit: &impl OrbitTrait) -> Vec<DVec3> {
     let mut vec: Vec<DVec3> = Vec::with_capacity(ORBIT_POLL_ANGLES);
 
     for i in 0..ORBIT_POLL_ANGLES {
-        let time = (i as f64) * orbit.get_orbital_period() / (ORBIT_POLL_ANGLES as f64);
+        let time = if orbit.get_eccentricity() < 1.0 {
+            (i as f64) * orbit.get_orbital_period() / (ORBIT_POLL_ANGLES as f64)
+        } else {
+            (i as f64) * TAU / (ORBIT_POLL_ANGLES as f64)
+        };
         vec.push(orbit.get_position_at_time(time));
     }
 
@@ -78,7 +82,7 @@ fn poll_flat(orbit: &impl OrbitTrait) -> Vec<DVec2> {
     let mut vec: Vec<DVec2> = Vec::with_capacity(ORBIT_POLL_ANGLES);
 
     for i in 0..ORBIT_POLL_ANGLES {
-        let angle = (i as f64) * orbit.get_orbital_period() / (ORBIT_POLL_ANGLES as f64);
+        let angle = (i as f64) * TAU / (ORBIT_POLL_ANGLES as f64);
         vec.push(orbit.get_pqw_position_at_true_anomaly(angle));
     }
 
@@ -88,7 +92,7 @@ fn poll_transform(orbit: &impl OrbitTrait) -> Vec<DVec3> {
     let mut vec: Vec<DVec3> = Vec::with_capacity(ORBIT_POLL_ANGLES);
 
     for i in 0..ORBIT_POLL_ANGLES {
-        let angle = (i as f64) * orbit.get_orbital_period() / (ORBIT_POLL_ANGLES as f64);
+        let angle = (i as f64) * TAU / (ORBIT_POLL_ANGLES as f64);
         vec.push(orbit.transform_pqw_vector(DVec2::new(1.0 * angle.cos(), 1.0 * angle.sin())));
     }
 
@@ -98,8 +102,13 @@ fn poll_eccentric_anomaly(orbit: &impl OrbitTrait) -> Vec<f64> {
     let mut vec: Vec<f64> = Vec::with_capacity(ORBIT_POLL_ANGLES);
 
     for i in 0..ORBIT_POLL_ANGLES {
-        let angle = (i as f64) * orbit.get_orbital_period() / (ORBIT_POLL_ANGLES as f64);
-        vec.push(orbit.get_eccentric_anomaly_at_mean_anomaly(angle));
+        let time = if orbit.get_eccentricity() < 1.0 {
+            (i as f64) * orbit.get_orbital_period() / (ORBIT_POLL_ANGLES as f64)
+        } else {
+            (i as f64) * TAU / (ORBIT_POLL_ANGLES as f64)
+        };
+
+        vec.push(orbit.get_eccentric_anomaly_at_time(time));
     }
 
     return vec;
@@ -107,28 +116,52 @@ fn poll_eccentric_anomaly(orbit: &impl OrbitTrait) -> Vec<f64> {
 fn poll_speed(orbit: &impl OrbitTrait) -> Vec<f64> {
     (0..ORBIT_POLL_ANGLES)
         .into_iter()
-        .map(|i| (i as f64) * orbit.get_orbital_period() / (ORBIT_POLL_ANGLES as f64))
+        .map(|i| {
+            if orbit.get_eccentricity() < 1.0 {
+                (i as f64) * orbit.get_orbital_period() / (ORBIT_POLL_ANGLES as f64)
+            } else {
+                (i as f64) * TAU / (ORBIT_POLL_ANGLES as f64)
+            }
+        })
         .map(|t| orbit.get_speed_at_time(t))
         .collect()
 }
 fn poll_flat_vel(orbit: &impl OrbitTrait) -> Vec<DVec2> {
     (0..ORBIT_POLL_ANGLES)
         .into_iter()
-        .map(|i| (i as f64) * orbit.get_orbital_period() / (ORBIT_POLL_ANGLES as f64))
+        .map(|i| {
+            if orbit.get_eccentricity() < 1.0 {
+                (i as f64) * orbit.get_orbital_period() / (ORBIT_POLL_ANGLES as f64)
+            } else {
+                (i as f64) * TAU / (ORBIT_POLL_ANGLES as f64)
+            }
+        })
         .map(|t| orbit.get_pqw_velocity_at_time(t))
         .collect()
 }
 fn poll_vel(orbit: &impl OrbitTrait) -> Vec<DVec3> {
     (0..ORBIT_POLL_ANGLES)
         .into_iter()
-        .map(|i| (i as f64) * orbit.get_orbital_period() / (ORBIT_POLL_ANGLES as f64))
+        .map(|i| {
+            if orbit.get_eccentricity() < 1.0 {
+                (i as f64) * orbit.get_orbital_period() / (ORBIT_POLL_ANGLES as f64)
+            } else {
+                (i as f64) * TAU / (ORBIT_POLL_ANGLES as f64)
+            }
+        })
         .map(|t| orbit.get_velocity_at_time(t))
         .collect()
 }
 fn poll_sv(orbit: &impl OrbitTrait) -> Vec<StateVectors> {
     (0..ORBIT_POLL_ANGLES)
         .into_iter()
-        .map(|i| (i as f64) * orbit.get_orbital_period() / (ORBIT_POLL_ANGLES as f64))
+        .map(|i| {
+            if orbit.get_eccentricity() < 1.0 {
+                (i as f64) * orbit.get_orbital_period() / (ORBIT_POLL_ANGLES as f64)
+            } else {
+                (i as f64) * TAU / (ORBIT_POLL_ANGLES as f64)
+            }
+        })
         .map(|t| orbit.get_state_vectors_at_time(t))
         .collect()
 }
